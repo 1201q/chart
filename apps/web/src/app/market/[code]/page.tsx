7870468977c40da495880477f6c711de80302f36
hwang <1201q@naver.com>
2025-12-20 12:40:29 +0900

    ✨ feat: 마켓 페이지 리다이렉트 및 존재하지 않는 마켓 처리 컴포넌트 추가

--- apps/web/src/app/market/[code]/page.tsx
+++ apps/web/src/app/market/[code]/page.tsx
@@ -10,8 +10,32 @@ import CoinInfo from '@/components/coinInfo/CoinInfo';
 import { OrderFormProvider } from '@/components/provider/OrderFormProvider';
 import OrderFormInit from '@/components/provider/OrderFormInit';
 
+import { MarketInfo as MarketInfoType } from '@chart/shared-types';
+import { notFound } from 'next/navigation';
+
+const getMarkets = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/markets`);
+
+  if (!res.ok) {
+    return [];
+  }
+
+  const json = (await res.json()) as MarketInfoType[];
+
+  const symbols = json.map((market) => market.code);
+
+  return symbols;
+};
+
 export default async function Page({ params }: { params: Promise<{ code: string }> }) {
   const { code } = await params;
+
+  const markets = await getMarkets();
+
+  // 유효한 심볼인지 검사
+  if (!markets.includes(code)) {
+    notFound();
+  }
 
   return (
     <div className={styles.page}>
