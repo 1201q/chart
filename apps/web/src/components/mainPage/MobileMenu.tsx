4ec797f4ac0d946bfc9df1ed0f196ba9e8af129b
1201q <1201q@naver.com>
2026-03-13 11:35:53 +0900

    ✨ feat: 계정설정 메뉴 제거

--- apps/web/src/components/mainPage/MobileMenu.tsx
+++ apps/web/src/components/mainPage/MobileMenu.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import styles from './styles/mobile.menu.module.css';
-import { X, Moon, Sun, LogOut, Settings } from 'lucide-react';
+import { X, Moon, Sun, LogOut } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
@@ -101,12 +101,6 @@ const MobileMenu = ({ isOpen, onClose, user }: MobileMenuProps) => {
           </div>
 
           {/* <div className={styles.sectionDivider} /> */}
-
-          {/* 계정 설정 */}
-          <Link href="/account/settings" className={styles.userAction} onClick={onClose}>
-            <Settings size={18} />
-            <span>계정 설정</span>
-          </Link>
 
           {/* 로그아웃 */}
           <button
